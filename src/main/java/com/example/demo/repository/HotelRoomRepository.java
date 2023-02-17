574e64c6e79824b528b65997de7c7aaf27fa20d3
Giancarlo1021 <giancarloavila725@gmail.com>
2023-02-17 16:18:42 -0700

    everything from demo API, to be worked on here further

---
+++ src/main/java/com/example/demo/repository/HotelRoomRepository.java
@@ -0,0 +1,41 @@
+package com.example.demo.repository;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import com.example.demo.entity.HotelRooms;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HotelRoomRepository {
+
+    @Autowired
+    private DynamoDBMapper dynamoDBMapper;
+
+    public HotelRooms save(HotelRooms hotelRooms) {
+        dynamoDBMapper.save(hotelRooms);
+        return hotelRooms;
+    }
+
+    public HotelRooms getHotelRoomsById(String roomNumber) {
+        return dynamoDBMapper.load(HotelRooms.class, roomNumber);
+    }
+
+    public String delete(String roomNumber) {   // delete roomNumber --> all of the db entry --> customer, employee
+        HotelRooms emp = dynamoDBMapper.load(HotelRooms.class, roomNumber);
+        dynamoDBMapper.delete(emp);
+        return "Deleted!";
+    }
+
+    public HotelRooms update(String roomNumber, HotelRooms hotelRooms) {
+        dynamoDBMapper.save(hotelRooms,
+                new DynamoDBSaveExpression()
+                        .withExpectedEntry("roomNumber",
+                                new ExpectedAttributeValue(
+                                        new AttributeValue().withS(roomNumber)
+                                )));
+        return dynamoDBMapper.load(HotelRooms.class, roomNumber);
+    }
+}
