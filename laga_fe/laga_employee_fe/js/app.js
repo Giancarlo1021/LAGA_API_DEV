42de1cc4a846d44d07657fe68d30daaef45ecfad
leojacinto.magtibay.103@my.csun.edu <leejay.magtibay11@gmail.com>
2023-04-12 17:28:43 -0700

    grokking towards code integration, unfinished

--- laga_fe/laga_employee_fe/js/app.js
+++ laga_fe/laga_employee_fe/js/app.js
@@ -1,1 +1,18 @@
 // postgres stuff goes here
+const { Pool, Client } = require("pg");
+
+const credentials = {
+  user: "yddhfzehsbkgyt",
+  host: "ec2-54-211-177-159.compute-1.amazonaws.com",
+  database: "d7iooqqs5al1us",
+  password: "3fde8d9ff4d0b5dc86b32742cd6ecb30f57407f15fd0f22f1f7c014b60970470",
+  port: 5432,
+};
+
+async function poolDemo() {
+  const pool = new Pool(credentials);
+  const now = await pool.query("SELECT NOW()");
+  await pool.end();
+
+  return now;
+}
